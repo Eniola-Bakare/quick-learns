29ab4a66e464716d806e83a3dcdb50bf1765df1c
eniolaa <eebakare@gmail.com>
2024-08-09 14:30:59 +0100

    working with react query: using use query

--- src/app/reactquery/page.tsx
+++ src/app/reactquery/page.tsx
@@ -1,3 +1,26 @@
+'use client'
+
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+
 export default function ReactQuery() {
-return <div>ReactQuery !</div>
+  const { data, isLoading, error, isError } = useQuery({
+    queryKey: ["superheroes"],
+    queryFn: () =>
+      fetch(`http://localhost:4000/superheroes`).then((res) => res.json()),
+  });
+
+  if (isLoading) return <h3 className=" italic">Loading...</h3>;
+  if (isError) return <h3 className=" italic">ERROR: {error.message}</h3>;
+  return (
+    <>
+      ReactQuery !
+      <div>
+        {data.map((each: any) => (
+          <h2 key={each.id}>
+            {each.id}: {each.name}
+          </h2>
+        ))}
+      </div>
+    </>
+  );
 }
